7a5c2a53648d91c6d029659ba560db342922a8bd
Angel's Work Station (Coding) <angelsworkstation@Angels-iMac.local>
2020-10-28 08:36:25 -0700

    [github.js] Add new element and object to page

--- github.js
+++ github.js
@@ -1,9 +1,21 @@
+const container = document.querySelector('.container');
+
+
 fetch('https://api.github.com/users/ABarranco95')
 .then(response => {
     return response.json();
 })
 .then(githubData => {
-    console.log(githubData)
+    const angelObj = {
+        bio: githubData.bio,
+        username: githubData.login,
+        name: githubData.name,
+    }
+    console.log(angelObj)
+
+    const newElement = document.createElement('p');
+    newElement.textContent = angelObj.name;
+    container.appendChild(newElement)
 })
 .catch(error => {
     console.log(error);
